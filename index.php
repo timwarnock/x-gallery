25e72faa31f6109859b1f1b320a766bb93d49035
Timothy Warnock <tim@timwarnock.com>
2012-03-20 12:41:07 -0700

    json feed

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
+/*
+ * api feed
+ * 
+ * /json or /xml (default)
+ *
+ */
+
 if (!file_exists('cache/META.xml')) {
+
+  ## create META.xml
   $RAW = `cat */META.xml | grep -v '<?xml'`;
   $META = new SimpleXMLElement('<projects>'.$RAW.'</projects>');
   $dom = dom_import_simplexml($META)->ownerDocument;
   $dom->formatOutput = true;
   file_put_contents("cache/META.xml", $dom->saveXML());
+ 
+  ## create META.json
+  $META = simplexml_load_file('cache/META.xml');
+  file_put_contents('cache/META.json', json_encode($META)); 
 }
 
-print( file_get_contents('cache/META.xml'));
+if ($_REQUEST['view'] == 'json') {
+	print( file_get_contents('cache/META.json'));
+} else {
+	print( file_get_contents('cache/META.xml'));
+}
 ?>
